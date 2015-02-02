8993049d7546210a28bc5b4f0ca57b8b1a9befea
Anwar A. Ruff <anwarruff@gmail.com>
2015-02-01 21:53:38 -0500

    Created the Questionnaire controller

---
+++ app/Http/Controllers/Questionnaire.php
@@ -0,0 +1,16 @@
+<?php namespace Officium\Http\Controllers;
+
+use Officium\Http\Requests;
+use Officium\Http\Controllers\Controller;
+
+use Illuminate\Http\Request;
+
+class Questionnaire extends Controller
+{
+    public function incomingQuestionnaire()
+    {
+        return '';
+    }
+
+
+}
