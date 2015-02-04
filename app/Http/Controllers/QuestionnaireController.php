32a14a3779c338647a60da3818d6988441bda843
Anwar A. Ruff <anwarruff@gmail.com>
2015-02-04 15:17:00 -0500

    Renamed questionnaire controller

--- app/Http/Controllers/QuestionnaireController.php
+++ app/Http/Controllers/QuestionnaireController.php
@@ -5,12 +5,10 @@ use Officium\Http\Controllers\Controller;
 
 use Illuminate\Http\Request;
 
-class Questionnaire extends Controller
+class QuestionnaireController extends Controller
 {
-    public function incomingQuestionnaire()
+    public function incoming()
     {
-        return '';
+        return view('pages.subject.questionnaire.incoming');
     }
-
-
 }
