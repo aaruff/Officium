494a31131cb1f19fcbd32c0b0580ffb453ee05ad
Anwar A. Ruff <anwarruff@gmail.com>
2015-02-09 23:51:53 -0500

    Added question to questionnaire form

--- resources/views/pages/subject/questionnaire/incoming.blade.php
+++ resources/views/pages/subject/questionnaire/incoming.blade.php
@@ -22,6 +22,7 @@
             <input type="text" id="course_load" value = "{{old('course_load')}}">
         </div>
     </div>
+
     {{--  QUESTION 2 --}}
     <div class="control-group">
         <label class="control-label">What is your major?</label>
@@ -29,6 +30,7 @@
             <input type="text" id="major" value = "{{old('major')}}">
         </div>
     </div>
+
     {{--  QUESTION 3 --}}
     <div class="control-group">
         <label class="control-label">What is your GPA?</label>
@@ -36,20 +38,43 @@
             <input type="text" id="gpa" value = "{{old('gpa')}}">
         </div>
     </div>
+
     {{--  QUESTION 4 --}}
     <div class="control-group">
         <label class="control-label">Over the course of the next week how many of each of the following do you have:</label>
+
+        {{--  QUESTION 4.1: Entry --}}
         <label class="control-label">Minor Assignments?</label>
         <div class="controls">
             <input type="text" id="minor_assignments" value = "{{old('minor_assignments')}}">
         </div>
+        {{--  QUESTION 4.1: Dates --}}
         <label class="control-label">Enter the deadlines for the above minor assignments</label>
+        <div class="controls">
+            <input type="text" id="minor_assignment_dates" value = "{{old('minor_assignment_dates')}}">
+        </div>
 
+        {{--  QUESTION 4.2: Entry --}}
         <label class="control-label">Major assignments / term papers?</label>
         <div class="controls">
             <input type="text" id="major_assignments" value = "{{old('major_assignments')}}">
         </div>
+        {{--  QUESTION 4.2: Dates --}}
         <label class="control-label">Enter the deadlines for the above major assignments</label>
+        <div class="controls">
+            <input type="text" id="major_assignment_dates" value = "{{old('minor_assignment_dates')}}">
+        </div>
+
+        {{--  QUESTION 4.3: Entry --}}
+        <label class="control-label">Exams?</label>
+        <div class="controls">
+            <input type="text" id="exams" value = "{{old('exams')}}">
+        </div>
+        {{--  QUESTION 4.2: Dates --}}
+        <label class="control-label">Enter the deadlines for the above major assignments</label>
+        <div class="controls">
+            <input type="text" id="exams" value = "{{old('exams')}}">
+        </div>
     </div>
 
     {!! Form::close() !!}
